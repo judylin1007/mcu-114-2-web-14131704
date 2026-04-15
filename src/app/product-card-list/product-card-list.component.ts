b71a1b2558136a5b69292dfb2eea5dfa41ea00fb
judylin <judy159703@gmail.com>
2026-04-15 16:53:44 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,14 +1,17 @@
 import { Product } from '../model/product';
+import { PaginationComponent } from '../pagination/pagination.component';
 import { ProductCardComponent } from './../product-card/product-card.component';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card-list',
-  imports: [ProductCardComponent],
+  imports: [PaginationComponent, ProductCardComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
   @Input({ required: true })
   products!: Product[];
+
+  pageIndex = 1;
 }
