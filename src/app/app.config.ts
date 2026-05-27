b59435f530637d6abe2992a2be59ba17d6430da1
judylin <judy159703@gmail.com>
2026-05-27 17:07:53 +0800

    feat(product): 實作從 azure 服務取得產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,13 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { ProductService } from './services/product.service';
-import { ProductRemoteService } from './services/product-remote.service';
+import { ProductAzureService } from './services/product-azure.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideRouter(routes, withComponentInputBinding()),
-    { provide: ProductService, useClass: ProductRemoteService },
+    { provide: ProductService, useClass: ProductAzureService },
   ],
 };
